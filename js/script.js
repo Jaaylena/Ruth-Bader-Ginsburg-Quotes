24d2fbc6840e9b48d8a0b676a3b15f58655a08e9
Jelena Feliciano <68608110+Jaaylena@users.noreply.github.com>
2020-10-04 14:02:52 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,18 +10,46 @@ project 1 - A Random Quote Generator
 /*** 
  * create an array of quotes 
 ***/
-
+let randomQuote;
+const ruthQuotes = [
+  {Quote: "Don't be distracted by emotions like anger, envy, resentment. These just zap energy and waste time",
+source: "My Own Words"},
+  {Quote: "Woman will have achieved true equality when men share with them the responsibility of bringing up the next generation.",
+source:"an interview with the New York City Bar Associations",
+year: 2001},
+  {Quote: "Fight for the things you care about, but do it in a way that will lead others to join you.",
+source: "Luncheon at Harvard",
+year: 2015},
+{Quote:"If you want to be a true professional, you must do something outside yourself.",
+source: "My own words"}
+];
 
 
 /***
- * `getRandomQuote` function
+ *  a function that pulls the above quotes at random
 ***/
-
-
+function randomRuthQuote () {
+  const quoteIndex = Math.floor(Math.random() * ruthQuotes.length);
+  randomQuote = ruthQuotes[quoteIndex];
+    return randomQuote;
+}
 
 /***
- * `printQuote` function
+ * a `printQuote` function that selects each quote to display
 ***/
+function printQuote () {
+  //call randomRuthQuote function
+  randomRuthQuote(); 
+  let HTML = '';
+  HTML += '<p class="Quote">' + randomQuote.Quote;
+  HTML += '<p class="source">' + randomQuote.source;
+  //a conditional statement that checks if the quote incluedes a year 
+  if(randomQuote.year) {
+    HTML += '<span>, </span>' + randomQuote.year; + '</p>';
+  }
+  document.getElementById('quote-box').innerHTML = HTML;
+  
+}
 
 
 
